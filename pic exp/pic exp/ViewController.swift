3ffb6479ec36cf49c14d44107d69effe1fa882c3
Alexis Graves <45857879+avlon96@users.noreply.github.com>
2020-05-16 21:07:45 -0400

    Add files via upload

---
+++ pic exp/pic exp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  pic exp
+//
+//  Created by Alexis Laston on 5/16/20.
+//  Copyright © 2020 Lillie VotBot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func experiment(sender: UIButton) {
+   
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+    }
+    }
+
+    
+    
+
+
+
+
